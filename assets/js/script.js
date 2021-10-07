1a452a753bd9eca9246e060ac654244cad14dd1a
GitHub-Harrison <harrison-johnson@outlook.com>
2021-10-07 12:54:15 +0000

    Add comments to JavaScript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,28 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 
 let randomQuestions, currentQuestion;
 
+// code to make the start and next button work
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
     currentQuestion++
     nextQuestion()
 })
-
+//  function which runs the game
 function startGame() {
-
     startButton.classList.add('hidden')
     randomQuestions = questions.sort(() => Math.random() - .5)
     currentQuestion = 0
     questionContainerElement.classList.remove('hidden')
     nextQuestion()
-
 }
 
+// function to give the user the next question
 function nextQuestion() {
     reset()
     getQuestion(randomQuestions[currentQuestion])
 }
 
+// function to get the questions from the array
 function getQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -41,6 +42,7 @@ function getQuestion(question) {
     })
 }
 
+// function to reset after the question has been answered
 function reset() {
     nextButton.classList.add('hidden')
     while (answerButtonsElement.firstChild) {
@@ -48,6 +50,11 @@ function reset() {
     }
 }
 
+/* 
+function that checks the answer the user selects and checks if its correct or not, 
+this function also checks if there are more questions in the array or not to 
+either show a 'next' or 'play again' button
+*/
 function checkAnswer(event) {
     const selectedAnswer = event.target
     const correct = selectedAnswer.dataset.correct
@@ -62,6 +69,7 @@ function checkAnswer(event) {
     }
 }
 
+// function to set the colours for both correct and incorrect answers (green = correct, red = incorrect)
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -71,11 +79,13 @@ function setStatusClass(element, correct) {
     }
 }
 
+// function that removes the colour effect of getting the answer right or wrong
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('incorrect')
 }
 
+// function to add score for each correct answer
 function addScore() {
 
 }
