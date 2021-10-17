d35af845022e9918f8e90b675e531053c5e3f1ee
GitHub-Harrison <harrison-johnson@outlook.com>
2021-10-17 12:09:22 +0000

    Fix spelling mistakes

--- assets/js/script.js
+++ assets/js/script.js
@@ -796,7 +796,7 @@ const questions = [{
                 correct: false
             },
             {
-                text: 'Emergy',
+                text: 'Energy',
                 correct: true
             }
         ]
